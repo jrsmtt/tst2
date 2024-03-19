fb4002696d9c6f2b966845175d9e03f3dc6c84fd
jrsmtt <163967198+jrsmtt@users.noreply.github.com>
2024-03-19 20:07:27 +0530

    Update LineChart.js

--- LineChart.js
+++ LineChart.js
@@ -28,6 +28,17 @@ const LineChart = ({ data }) => {
       }
     });
 
+
+
+    const getRandomColor = () => {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
     return () => chart.destroy();
   }, [data]);
 
